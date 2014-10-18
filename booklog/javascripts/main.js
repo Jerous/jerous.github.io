9180dccab8639565b111610c7cb5d9db74f4cc3e
tlu37317 <tlu37317@gmail.com>
2014-10-18 03:12:57 +0000

    fix ajax

--- booklog/javascripts/main.js
+++ booklog/javascripts/main.js
@@ -4,7 +4,7 @@
 		url: 'https://www.mokoversity.com/1/post/tags/startup',
 	    success: function(response, jqXHR, textStatus) {
 			$('#postTemplate')
-				.tmpl(response.posts) //
+				.tmpl(response.posts)
 				.appendTo('#content'); 
 	    },
 	    complete: function(jqXHR, textStatus) {
