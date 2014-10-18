0575777e86bcab1c1c14ff3935fe4ffb04c54ed2
tlu37317 <tlu37317@gmail.com>
2014-10-18 02:42:53 +0000

    fix ajax

--- booklog/javascripts/main.js
+++ booklog/javascripts/main.js
@@ -1,30 +1,14 @@
 (function($) {
 	$.ajax({
 		dataType: 'json',
-		url: 'http://localhost:3000/1/post',
+		url: 'https://www.mokoversity.com/1/post/tags/startup',
 	    success: function(response, jqXHR, textStatus) {
 			$('#postTemplate')
 				.tmpl(response.posts)
 				.appendTo('#content');
 	    },
 	    complete: function(jqXHR, textStatus) {
-			$('[id=subject]').each(function(index) {
-				var me = $(this);
-				
-				me.on('click', function() {
-					var id = me.data('id');
-
-					$.ajax({
-						dataType: 'json',
-						url: 'http://localhost:3000/1/post/' + id,
-						success: function(response, jqXHR, textStatus) {
-							$('#content').html('<h2>' 
-								+ response.post.content 
-								+ '</h2>');
-						},
-					});
-				});
-			});
+			
 	    }
 	});
 }) ($);
