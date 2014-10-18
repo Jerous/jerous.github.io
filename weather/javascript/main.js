db60d8057516150be795145293f4d9ae2869f434
tlu37317 <tlu37317@gmail.com>
2014-10-18 03:24:19 +0000

    add weather

---
+++ weather/javascript/main.js
@@ -0,0 +1,21 @@
+/**
+* Created with jerous.github.io.
+* User: tlu37317
+* Date: 2014-10-18
+* Time: 03:19 AM
+* To change this template use Tools | Templates.
+*/
+(function($) {
+	$.ajax({
+		dataType: 'json',
+		url: 'http://api.openweathermap.org/data/2.5/weather?q=taipei',
+	    success: function(response, jqXHR, textStatus) {
+			$('#postTemplate')
+				.tmpl(response.weather)
+				.appendTo('#content'); 
+	    },
+	    complete: function(jqXHR, textStatus) {
+			
+	    }
+	});
+}) ($);
