1a96957d4aced67a02b48634eb8171392c6b852a
tlu37317 <tlu37317@gmail.com>
2014-10-18 05:07:46 +0000

    add weather

---
+++ weather/javascripts/main.js
@@ -0,0 +1,34 @@
+(function($) {
+	$.ajax({
+		dataType: 'json',
+		url: 'http://api.openweathermap.org/data/2.5/weather?q=Taipei',
+	    success: function(response, jqXHR, textStatus) {
+            var data = [];
+            var obj = {};
+            
+            //生成data內的物件
+            obj.temp = response.main.temp;
+            obj.humidity = response.main.humidity;
+            obj.name = response.name;
+            
+            // Weather Icons
+            switch (response.weather[0].main) {
+                case "Clouds": obj.weatherIcon = "wi-day-cloudy";
+            }
+            
+            // Celsius
+            obj.celsius = parseInt(response.main.temp - 273.15);
+            
+            //將物件放入陣列
+            data.push(obj);
+            
+			$('#postTemplate')
+				.tmpl(data)
+				.appendTo('#content');
+            
+            $('#weather-icon').addClass('wi-day-cloudy');
+	    },
+	    complete: function(jqXHR, textStatus) {
+	    }
+	});
+}) ($);
